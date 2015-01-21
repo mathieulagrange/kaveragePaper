1efc91fa26d447470be2b94eb412301eca78f915
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2015-01-21 11:49:55 +0100

    final version of results

--- code/simpleSwap/swapRun.py
+++ code/simpleSwap/swapRun.py
@@ -3,7 +3,7 @@ import os
 for m in ['kaverages', 'kkmeans']:
     for o in range(0, 10):
         for j in range(0, 10):
-            command = './'+m+' -m /data/simple/testSwap_'+str(o)+'.matrix -c 5 -o /data/simple/testSwap_'+m+str(o)+'_'+str(j);
+            command = '~/projects/houle/dev/tools/clusteringAlgorithms/kAverages/simple/'+m+' -m /data/simple/testSwap_'+str(o)+'.matrix -c 5 -o /data/simple/testSwapO3_'+m+str(o)+'_'+str(j);
             print command
             os.system(command);
 
--- code/simpleSwap/swapTest.m
+++ code/simpleSwap/swapTest.m
@@ -6,7 +6,7 @@ algo = {'kaverages', 'kkmeans'};
 %     for k=1:10
 %         fileName = ['/data/simple/testSwap_' algo{n} num2str(k-1) '.csv']
 %         runLog = textscan(fopen(fileName), '%f,%f, %f,%f',  'HeaderLines',2);
-%         
+%
 %         iterations(k, n) = runLog{1}(end);
 %         moved(k, n) = runLog{2}(end);
 %         time(k, n) =  runLog{3}(end);
@@ -15,30 +15,33 @@ algo = {'kaverages', 'kkmeans'};
 %         % log.iterationTime = runLog{4}(1:end-1);
 %     end
 % end
-% 
+%
 % plot((time))
-% 
+%
 % time'
 
 
-for n=1:4
+for n=1:6
     for k=1:10
-            for m=1:10
-                if n>2
-                    ext='No';
-                    alg{n} = [algo{mod(n-1, 2)+1} 'No'];
-                else
-                    ext='';
-                               alg{n} = [algo{mod(n-1, 2)+1}];
-                end
-                fileName = ['/data/simple/test' ext 'Swap_' algo{mod(n-1, 2)+1} num2str(k-1) '_' num2str(m-1) '.csv'];
-                fid = fopen(fileName);
-        runLog = textscan(fid, '%f,%f, %f,%f',  'HeaderLines',2);
-        fclose(fid);
-        iterations(k, n, m) = runLog{1}(end);
-        moved(k, n, m) = runLog{2}(end);
-        time(k, n, m) =  runLog{3}(end);
+        for m=1:10
+            if n>2
+                ext='NoSwap_';
+                alg{n} = [algo{mod(n-1, 2)+1} 'No'];
+            else
+                ext='Swap_';
+                alg{n} = [algo{mod(n-1, 2)+1}];
             end
+            if n>4
+                ext='SwapO3_';
+            end
+            fileName = ['/data/simple/test' ext '' algo{mod(n-1, 2)+1} num2str(k-1) '_' num2str(m-1) '.csv']
+            fid = fopen(fileName);
+            runLog = textscan(fid, '%f,%f, %f,%f',  'HeaderLines',2);
+            fclose(fid);
+            iterations(k, n, m) = runLog{1}(end);
+            moved(k, n, m) = runLog{2}(end);
+            time(k, n, m) =  runLog{3}(end);
+        end
         % log.iterationMoved = runLog{2}(1:end-1);
         % log.iterationCriterion = runLog{3}(1:end-1);
         % log.iterationTime = runLog{4}(1:end-1);
@@ -50,11 +53,11 @@ vtime = squeeze(var(time, [], 3));
 atime = squeeze(mean(time, 3));
 % errorbar(atime, vtime)
 clf
-lineStyle = {'-', '-','--', '--' };
-marker = {'o', '*','o', '*' };
-for k=1:4
-semilogy(atime(:, k), 'color', 'k', 'linestyle', lineStyle{k}, 'marker', marker{k}, 'markersize', 10)
-hold on
+lineStyle = {'-', '-','--', '--', ':', ':' };
+marker = {'o', '*','o', '*', 'o', '*' };
+for k=1:6
+    semilogy(atime(:, k), 'color', 'k', 'linestyle', lineStyle{k}, 'marker', marker{k}, 'markersize', 10)
+    hold on
 end
 hold off
 % plot(squeeze(mean(time, 3)))
@@ -66,4 +69,4 @@ set(gca, 'fontsize', 16)
 axis tight
 % legend(alg)
 % set(gca, 'xticklabel', [10000+1000*(0:9)])
-saveas(gcf, 'simpleSwap.png')
+% saveas(gcf, 'simpleSwap.png')
